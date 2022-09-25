ecc75cb46e5ed042b57d8d14480e6b4b78c35b63
Samuil <samuil.pavlevchev@gmail.com>
2022-09-25 11:18:50 +0100

    Some URL updates

--- 02 URLs and Views/departments_app/departments_app/departments/urls.py
+++ 02 URLs and Views/departments_app/departments_app/departments/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = ()
+from django.urls import path
+
+from departments_app.departments.views import sample_view, show_department_details
+
+urlpatterns = (
+    path('', sample_view),
+    path('<department_id>/', show_department_details),
+    path('int/<int:department_id>/', sample_view),
+)
--- 02 URLs and Views/departments_app/departments_app/departments/views.py
+++ 02 URLs and Views/departments_app/departments_app/departments/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def sample_view(request: HttpRequest, *args, **kwargs):
+    order_by = request.GET.get('order_by', 'name')
+    body = f'path: {request.path}, args={args}, kwargs={kwargs}, order_by: {order_by}'
+
+
+    return HttpResponse(body)
+
+
+def show_department_details(request: HttpRequest, department_id):
+    body = f'path: {request.path}, id: {department_id}'
+    return HttpResponse(body)
