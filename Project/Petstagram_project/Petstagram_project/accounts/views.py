1cf25e8308e6e77838721226b6aea28196643df1
Samuil <samuil.pavlevchev@gmail.com>
2022-10-04 13:00:42 +0100

    App setup with static templates, all views and css.

--- Project/Petstagram_project/Petstagram_project/accounts/views.py
+++ Project/Petstagram_project/Petstagram_project/accounts/views.py
@@ -9,14 +9,14 @@ def register_user(request):
     return render(request, 'accounts/register-page.html')
 
 
-def delete_user(request):
+def delete_user(request, pk):
     return render(request, 'accounts/profile-delete-page.html')
 
 
-def details_user(request):
+def details_user(request, pk):
     return render(request, 'accounts/profile-details-page.html')
 
 
-def edit_user(request):
+def edit_user(request, pk):
     return render(request, 'accounts/profile-edit-page.html')
 
