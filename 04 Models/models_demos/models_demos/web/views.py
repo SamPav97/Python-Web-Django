61866f7a8c3315104867dc6875ed08c2cb44271d
Samuil <samuil.pavlevchev@gmail.com>
2022-10-06 17:18:12 +0100

    Requesting and filtering data from DB

--- 04 Models/models_demos/models_demos/web/views.py
+++ 04 Models/models_demos/models_demos/web/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from models_demos.web.models import Employee, Department
+
+
+def index(request):
+    employees = [e for e in Employee.objects.all() if e.department_id == 1]
+    employees2 = Employee.objects.filter(department_id=1) \
+        .order_by('first_name')
+
+    # get must return a single object. Not more then one... and it does not return a promise but the object directly.
+    # By promise I mean that get does not return a query set. Get is eager and acts even if unused.
+    department = Department.objects.get(pk=1)
+    context = {
+        'employees': employees,
+        'employees2': employees2,
+        'department': department,
+    }
+
+    return render(request, 'index.html', context)
